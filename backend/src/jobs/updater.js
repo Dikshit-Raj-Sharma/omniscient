4c016a627197974f1186ccdc16f13f53881995e1
Dikshit Raj Sharma <214721236+Dikshit-Raj-Sharma@users.noreply.github.com>
2026-07-12 00:07:13 +0000

    feat(updater): enhance cron job to analyze prices and send deal alerts

--- backend/src/jobs/updater.js
+++ backend/src/jobs/updater.js
@@ -1,6 +1,8 @@
 import cron from "node-cron";
 import { pool } from "../db/index.js";
 import { scrapePrice } from "../utils/scraper.js";
+import { analyseDeal } from "../utils/dealScorer.js";
+import { sendDealAlert } from "../utils/mailer.js";
 
 const delay = (ms) => {
   return new Promise((resolve) => {
@@ -12,18 +14,27 @@ const delay = (ms) => {
 
 cron.schedule("*/2 * * * *", async () => {
   try {
-    const result = await pool.query(`select id,url from products`);
-    for(const product of result.rows){
-        console.log("Checking price for: " + product.url)
-        const price = await scrapePrice(product.url);
-        if(price){
-            await pool.query(`insert into price_history (product_id,price) values ($1, $2)`,[product.id,price]);
+    const result = await pool.query(`select name,id,url from products`);
+    for (const product of result.rows) {
+      console.log("Checking price for: " + product.url);
+      const price = await scrapePrice(product.url);
+      let dealStatus;
+      if (price) {
+        dealStatus = await analyseDeal(product.id, price);
+        console.log("Status: " + dealStatus);
+        if (dealStatus == "Good Deal!") {
+          await sendDealAlert(product.name, product.url, price);
         }
-        await delay(5000);
+        await pool.query(
+          `insert into price_history (product_id,price) values ($1, $2)`,
+          [product.id, price]
+        );
+      }
 
+      await delay(5000);
     }
   } catch (error) {
-    console.error("failed to fetch the row",error);
+    console.error("failed to fetch the row", error);
   }
 });
 
